ee3968aea1f3271f695817578ff74cb21df59be9
purplelemons-dev <me@joshsmith.dev>
2025-12-14 17:27:03 -0600

    day 2 part 1

---
+++ day02/parts.py
@@ -0,0 +1,24 @@
+with open("./input.txt", 'r') as f:
+    lines = f.readlines()
+
+line = lines[0].strip("\n")
+ranges = line.split(',')
+
+# Part 1
+invalid_IDs: list[int] = []
+
+for i in ranges:
+    start, stop = map(int, i.split('-'))
+    
+    for j in range(start, stop + 1):
+        number = str(j)
+        length = len(number)
+
+        # Skip odd length
+        if not length % 2:
+            halfway = length // 2
+            if number[halfway:] == number[:halfway]:
+                invalid_IDs.append(j)
+
+print(f"Part 1 output:\t{sum(invalid_IDs)}")
+
