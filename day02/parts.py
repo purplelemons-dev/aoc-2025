883e502b648efbcf6bf7c12ca24bdc469ed8c328
purplelemons-dev <me@joshsmith.dev>
2025-12-14 18:08:17 -0600

    day 2 part 2

--- day02/parts.py
+++ day02/parts.py
@@ -22,3 +22,24 @@ for i in ranges:
 
 print(f"Part 1 output:\t{sum(invalid_IDs)}")
 
+# Part 2
+invalid_IDs = []
+
+for i in ranges:
+    start, stop = map(int, i.split('-'))
+    
+    for j in range(start, stop + 1):
+        number = str(j)
+        length = len(number)
+        halfway = round(length/2)
+
+        for k in range(1, halfway + 1):
+            subsection = number[:k]
+            if number == (subsection * (length // k)):
+                # print(f"Adding {number=}\t{subsection=}\t{k=}")
+                invalid_IDs.append(j)
+                break
+
+# 36156785904   is too high
+print(f"Part 2 output:\t{sum(invalid_IDs)}")
+
