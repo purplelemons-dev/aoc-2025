604a985761f54b2cc51dbb3e069b211b9c413f26
purplelemons-dev <me@joshsmith.dev>
2025-12-22 18:39:25 -0600

    day 3 part 2

--- day03/parts.py
+++ day03/parts.py
@@ -28,3 +28,46 @@ for bank in lines:
 
 print(f"Part 1 output:\t{sum(banks)}")
 
+# Part 2
+
+def get_joltage(input: str, counter: int, current: int = 0) -> int | None:
+    if len(input) < counter:
+        return None
+
+    elif counter == 1:
+        return max(
+            current * 10 + int(i)
+            for i in input
+        )
+
+    else:
+        for num in numbers:
+            idx = input.find(num)
+
+            if idx >= 0:
+                next_joltage = get_joltage(
+                    input[idx + 1:],
+                    counter - 1,
+                    current * 10 + int(num)
+                )
+
+                if next_joltage == None:
+                    continue
+
+                else:
+                    return next_joltage
+
+            else:
+                continue
+
+
+part_2_banks: list[int] = []
+
+for bank in lines:
+    joltage = get_joltage(bank, 12)
+
+    if joltage != None:
+        part_2_banks.append(joltage)
+
+print(f"Part 2 output:\t{sum(part_2_banks)}")
+
