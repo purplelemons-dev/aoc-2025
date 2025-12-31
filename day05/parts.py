557125ccc2f95be2851260ef9cc7351e02068dd4
purplelemons-dev <me@joshsmith.dev>
2025-12-31 15:40:16 -0600

    day 5 part 2

--- day05/parts.py
+++ day05/parts.py
@@ -15,8 +15,38 @@ for i in ranges:
 
 total = 0
 for i in IDs:
-    if any(int(i) in rng for rng in parsed_ranges):
+    if any(int(i) in range(rng.start, rng.stop) for rng in parsed_ranges):
         total += 1
 
 print(f"Part 1 output:\t{total}")
+
+# Part 2
+
+newRanges: list[list[int]] = []
+
+sortedRanges: list[tuple[int, int]] = [
+    (rng.start, rng.stop)
+    for rng in parsed_ranges
+]
+
+sortedRanges.sort()
+
+newRanges.append(list(sortedRanges[0]))
+
+for start, stop in sortedRanges[1:]:
+    last_range = newRanges[-1]
+    prev_rng = range(last_range[0], last_range[1])
+
+    if stop in prev_rng and start in prev_rng:
+        continue
+    elif last_range[1] >= start:
+        newRanges[-1][1] = stop
+    elif last_range[1] < start:
+        newRanges.append([start, stop])
+    else:
+        raise RuntimeError(f"Undefined condition for: {prev_rng= } {start= } {stop= }")
+
+total2 = sum(len(range(start, stop)) for start, stop in newRanges)
+
+print(f"Part 2 output:\t{total2}")
 
